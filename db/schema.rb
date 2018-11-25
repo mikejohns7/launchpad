b1c55d7999d2f3f9a001a405bbeaee37ac7d9b56
Mike Johns <mikejohns7@icloud.com>
2018-11-24 16:44:41 -0800

    WIP cleaning up Clearance controller stuff

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_23_224907) do
+ActiveRecord::Schema.define(version: 2018_11_23_230110) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,11 @@ ActiveRecord::Schema.define(version: 2018_11_23_224907) do
     t.integer "is_admin"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "encrypted_password", limit: 128
+    t.string "confirmation_token", limit: 128
+    t.string "remember_token", limit: 128
+    t.index ["email"], name: "index_users_on_email"
+    t.index ["remember_token"], name: "index_users_on_remember_token"
   end
 
 end
